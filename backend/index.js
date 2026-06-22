d8936fe50e2259920a3311d5238ffd26787b8d82
alammd0 <sonualam96314@gmail.com>
2026-06-22 13:58:16 +0530

    setup

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+import app from './src/app.js';
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
